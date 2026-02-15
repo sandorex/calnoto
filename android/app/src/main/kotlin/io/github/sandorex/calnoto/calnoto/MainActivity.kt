a5c1dac6146cca51df0369e0651189ce9548ee13
sandorex <rzhw3h@gmail.com>
2026-02-15 12:02:29 +0100

    Working flutter app for android and linux

---
+++ android/app/src/main/kotlin/io/github/sandorex/calnoto/calnoto/MainActivity.kt
@@ -0,0 +1,5 @@
+package io.github.sandorex.calnoto.calnoto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- flake.nix
+++ flake.nix
@@ -1,13 +1,9 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, fenix, ...  }:
+  outputs = { self, nixpkgs }:
     let
       inherit self;
       system = "x86_64-linux";
@@ -25,15 +21,21 @@
       VERGEN_IDEMPOTENT = "1";
       VERGEN_GIT_SHA = if (self ? "rev") then (builtins.substring 0 7 self.rev) else "nix-dirty";
 
-      # NOTE this the godot version used to build the application
-      godot = pkgs.godotPackages_4_5;
-
       # copy all of these when copying from godot config.gradle
       # https://github.com/godotengine/godot/blob/4.5/platform/android/java/app/config.gradle
       androidVersion = {
-        buildTools = "35.0.1";
-        sdk = "35";
-        ndk = "28.1.13356709";
+        buildTools = "35.0.0";
+        sdk = "36";
+        ndk = "28.2.13676358";
+        cmake = "3.22.1";
+      };
+
+      androidComposition = pkgs.androidenv.composeAndroidPackages {
+        buildToolsVersions = [ androidVersion.buildTools ];
+        platformVersions = [ androidVersion.sdk ];
+        ndkVersions = [ androidVersion.ndk ];
+        includeNDK = true;
+        cmakeVersions = [ androidVersion.cmake ];
       };
     in
     {
@@ -43,8 +45,6 @@
         desktop = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             git
-            godot.godot
-            godot.export-templates-bin
 
             cargo
             rust-analyzer
@@ -56,54 +56,25 @@
           inherit VERGEN_IDEMPOTENT VERGEN_GIT_SHA;
         };
 
-        android =
-          let
-            androidComposition = pkgs.androidenv.composeAndroidPackages {
-              buildToolsVersions = [ androidVersion.buildTools ];
-              platformVersions = [ androidVersion.sdk ];
-              ndkVersions = [ androidVersion.ndk ];
-              # platformToolsVersion = ""; # NOTE not in config.gradle??
-              includeNDK = true;
+        android = pkgs.mkShell rec {
+          nativeBuildInputs = with pkgs; [
+            git
+            flutter
+            androidComposition.androidsdk
+            androidComposition.platform-tools
+            androidComposition.cmake
+            javaPackages.compiler.openjdk17
+          ];
 
-              # TODO idk if its needed
-              # includeExtras = [ "extras;google;auto" ];
-            };
+          ANDROID_HOME = "${androidComposition.androidsdk}/libexec/android-sdk";
+          ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
+          ANDROID_NDK_ROOT = "${ANDROID_HOME}/ndk-bundle";
+          GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_HOME}/build-tools/${androidVersion.buildTools}/aapt2";
 
-            pkgs' = pkgs.pkgsCross.aarch64-android-prebuilt;
-          in
-          pkgs'.mkShell rec {
-            nativeBuildInputs = with pkgs; [
-              git
-              # TODO godot does not find the templates
-              godot.godot
-              godot.export-templates
+          CARGO_BUILD_TARGET = "aarch64-linux-android";
 
-              (with fenix.packages.${system}; combine [
-                stable.toolchain
-                targets.aarch64-linux-android.stable.rust-std
-                targets.x86_64-linux-android.stable.rust-std # for emulator
-              ])
-
-              androidComposition.androidsdk
-              javaPackages.compiler.openjdk17
-
-              # cc crate looks for `cc` executable instead of $CC env var and so fails to build
-              (pkgs.linkFarm "gcc-link" [
-                {
-                  name = "bin/cc";
-                  path = "${pkgs'.stdenv.cc}/bin/${pkgs'.stdenv.cc.targetPrefix}cc";
-                }
-              ])
-            ];
-
-            ANDROID_HOME = "${androidComposition.androidsdk}/libexec/android-sdk";
-            ANDROID_NDK_ROOT = "${ANDROID_HOME}/ndk-bundle";
-            GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_HOME}/build-tools/${androidVersion.buildTools}/aapt2";
-
-            CARGO_BUILD_TARGET = "aarch64-linux-android";
-
-            inherit VERGEN_IDEMPOTENT VERGEN_GIT_SHA;
-          };
+          inherit VERGEN_IDEMPOTENT VERGEN_GIT_SHA;
+        };
       };
     };
 }
