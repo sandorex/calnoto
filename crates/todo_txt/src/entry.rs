7d549f2561a6f5015797ac31a1c44fb12f91d679
sandorex <rzhw3h@gmail.com>
2026-02-16 21:53:47 +0100

    crates/todo_txt: better description parser

--- crates/todo_txt/src/entry.rs
+++ crates/todo_txt/src/entry.rs
@@ -1,8 +1,18 @@
-use std::collections::HashMap;
+use std::{collections::HashMap};
 use chrono::NaiveDate;
 
-// TODO figure out what to do as raw description cannot be retrieved from
-// processed one as tags are used in middle of the description
+/// Parsed metadata from description
+///
+/// Contains tags and custom properties
+#[derive(Debug, Default, PartialEq, Eq, Clone)]
+pub struct TodoEntryMetadata {
+    /// Processed description without tag prefixes or properties
+    pub description: String,
+    pub projects: Vec<String>,
+    pub contexts: Vec<String>,
+    pub properties: HashMap<String, String>,
+}
+
 #[derive(Debug)]
 pub struct TodoEntry {
     pub completed: bool,
@@ -13,83 +23,58 @@ pub struct TodoEntry {
     pub creation_date: Option<NaiveDate>,
 
     /// Raw unprocessed description of the task
-    pub(crate) raw_description: String,
-
-    // these are parsed from raw_description
-    /// Clean description without tag prefix or special properties
-    pub(crate) description: String,
-    pub(crate) projects: Vec<String>,
-    pub(crate) contexts: Vec<String>,
-    pub(crate) properties: HashMap<String, String>,
+    pub description: String,
 }
 
-// TODO really plan out when and where to complain about malformed description
 impl TodoEntry {
-    pub fn new(completed: bool, priority: Option<char>, completion_date: Option<NaiveDate>, creation_date: Option<NaiveDate>, description: String) -> Self {
-        let mut entry = Self {
-            completed,
-            priority,
-            completion_date,
-            creation_date,
-            description,
-            ..Default::default()
-        };
+    /// Parses description and returns metadata that contains clean description,
+    /// tags and properties
+    pub fn metadata(&self) -> TodoEntryMetadata {
+        let mut metadata = TodoEntryMetadata::default();
 
-        // TODO Figure out if i want to return result here
-        entry.parse_description().unwrap();
+        // NOTE manually parsing tags and properties, simpler than using nom tbh
+        for word in self.description.split_ascii_whitespace() {
+            let clean = match word.chars().next().unwrap() {
+                // project tag
+                '+' => {
+                    let clean = &word[1..];
+                    metadata.projects.push(clean.to_string());
 
-        entry
-    }
+                    clean
+                },
 
-    // TODO this needs a proper error type!
-    /// Set description for the entry
-    pub fn set_description(&mut self, description: &str) -> Result<(), String> {
-        self.raw_description = description.to_string();
-        self.parse_description().map_err(|err| format!("{err}"))?;
+                // context tag
+                '@' => {
+                    let clean = &word[1..];
+                    metadata.contexts.push(clean.to_string());
 
-        Ok(())
-    }
+                    clean
+                },
 
-    /// Updates internal values for projects, contexts, properties and clean description
-    fn parse_description(&mut self) -> nom::IResult<&str, ()> {
-        let (leftover, (description, projects, contexts, properties)) = crate::parser::parse_description(&self.raw_description)?;
+                // properties or just normal text
+                _ => match word.split_once(":") {
+                    Some((key, value)) => {
+                        if key.is_empty() || value.is_empty() {
+                            word
+                        } else {
+                            metadata.properties.insert(key.to_string(), value.to_string());
 
-        self.description = description;
-        self.projects = projects;
-        self.contexts = contexts;
-        self.properties = properties;
+                            // do not add properties to description
+                            ""
+                        }
+                    },
+                    _ => word,
+                },
+            };
 
-        Ok((leftover, ()))
-    }
+            if !clean.is_empty() {
+                metadata.description += &format!("{clean} ");
+            }
+        }
 
-    /// Get raw description that includes tags and properties
-    pub fn raw_description(&self) -> &str {
-        self.raw_description.as_str()
-    }
+        metadata.description = metadata.description.trim().to_string();
 
-    /// Get description with tags (prefixes and properties removed)
-    pub fn description(&self) -> &str {
-        self.description.as_str()
-    }
-
-    /// Return projects of todo entry
-    pub fn projects(&self) -> &Vec<String> {
-        &self.projects
-    }
-
-    /// Return contexts of todo entry
-    pub fn contexts(&self) -> &Vec<String> {
-        &self.contexts
-    }
-
-    /// Get all properties defined
-    pub fn properties(&self) -> &[(&str, &str)] {
-        todo!()
-    }
-
-    /// Get single property by name
-    pub fn property(&self, key: &str) -> Option<&str> {
-        self.properties.get(key).map(|x| x.as_str())
+        metadata
     }
 }
 
@@ -100,11 +85,7 @@ impl Default for TodoEntry {
             priority: None,
             completion_date: None,
             creation_date: None,
-            raw_description: "".to_string(),
             description: "".to_string(),
-            projects: vec![],
-            contexts: vec![],
-            properties: HashMap::new(),
         }
     }
 }
@@ -130,7 +111,7 @@ impl Into::<String> for &TodoEntry {
             output += &format!("{} ", creation_date);
         }
 
-        output += &format!("{} ", self.raw_description);
+        output += &format!("{} ", self.description);
 
         // remove any leftover whitespace
         output.trim_end().to_string()
@@ -140,5 +121,41 @@ impl Into::<String> for &TodoEntry {
 impl Into::<String> for TodoEntry {
     fn into(self) -> String {
         return Into::<String>::into(&self)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn todo_entry_metadata() {
+        let entry = TodoEntry {
+            description: "My good @friend +jack is coming over time:2pm".to_string(),
+            ..Default::default()
+        };
+
+        assert_eq!(entry.metadata(), TodoEntryMetadata {
+            description: "My good friend jack is coming over".to_string(),
+            projects: vec!["jack".to_string()],
+            contexts: vec!["friend".to_string()],
+            properties: HashMap::from([
+                ("time".to_string(), "2pm".to_string())
+            ])
+        });
+
+        // test parsing of partial properties and weird characters
+        let entry = TodoEntry {
+            description: "A num:1 tricky: o+ne w+ith+ :multiple malf@rmed we1.;rd things??!?".to_string(),
+            ..Default::default()
+        };
+
+        assert_eq!(entry.metadata(), TodoEntryMetadata {
+            description: "A tricky: o+ne w+ith+ :multiple malf@rmed we1.;rd things??!?".to_string(),
+            properties: HashMap::from([
+                ("num".to_string(), "1".to_string())
+            ]),
+            ..Default::default()
+        });
     }
 }
--- crates/todo_txt/src/parser.rs
+++ crates/todo_txt/src/parser.rs
@@ -1,5 +1,5 @@
 use nom::branch::alt;
-use nom::character::complete::{alpha1, alphanumeric1, anychar, char, digit1, multispace0, multispace1, not_line_ending, one_of};
+use nom::character::complete::{alpha1, alphanumeric1, anychar, char, digit1, line_ending, multispace0, multispace1, not_line_ending, one_of};
 use nom::{IResult, Parser};
 use nom::combinator::{eof, map_res, not, opt, recognize, rest, success, value};
 use nom::bytes::complete::{take_till, take_while1, is_not, take, take_while};
@@ -33,71 +33,6 @@ pub fn parse_date(input: &str) -> IResult<&str, NaiveDate> {
     Ok((leftover, NaiveDate::from_ymd_opt(year.into(), month.into(), day.into()).expect("Invalid calendar day {year}-{month}-{day}")))
 }
 
-pub fn parse_description(input: &str) -> IResult<&str, (String, Vec<String>, Vec<String>, HashMap<String, String>)> {
-    let mut description: String = String::new();
-    let mut projects: Vec<String> = vec![];
-    let mut context: Vec<String> = vec![];
-    let mut properties: HashMap<String, String> = HashMap::new();
-
-    let (leftover, parsed) = many1((
-        alt((
-            // +project @context
-            (
-                one_of("+@"),
-                take_till(|x: char| x.is_whitespace()),
-            ),
-
-            // key:val properties
-            (
-                success(':'),
-                recognize((
-                    take_till(|x: char| x == ':' || x.is_whitespace()),
-                    char(':'),
-                    take_till(|x: char| x.is_whitespace()),
-                )),
-            ),
-
-            // other
-            (
-                success('?'),
-                value("", take_till(|x: char| x.is_whitespace())),
-            )
-        )),
-        value("", multispace1),
-    ))
-    .parse(input)?;
-
-    for ((ch, text), _) in parsed {
-        match ch {
-            '+' => {
-                description += &format!("+{text} ");
-                projects.push(text.to_string());
-            },
-            '@' => {
-                description += &format!("@{text} ");
-                context.push(text.to_string());
-            },
-            ':' => match text.split_once(":") {
-                Some((k, v)) => {
-                    properties.insert(k.to_string(), v.to_string());
-                }
-
-                // ignore invalid properties
-                _ => {
-                    description += &format!("{text} ");
-                },
-            },
-
-            _ => {
-                // preserve the origina description
-                description += &format!("{text} ");
-            },
-        }
-    }
-
-    Ok((leftover, (description.trim().to_string(), projects, context, properties)))
-}
-
 pub fn parse_entry(input: &str) -> IResult<&str, TodoEntry> {
     let (leftover, (completed, priority, completion_date, creation_date, description)) = (
         // parse completion mark
@@ -116,7 +51,23 @@ pub fn parse_entry(input: &str) -> IResult<&str, TodoEntry> {
         not_line_ending,
     ).parse(input)?;
 
-    Ok((leftover, TodoEntry::new(completed, priority, completion_date, creation_date, description.to_string())))
+    Ok((leftover, TodoEntry {
+        completed,
+        priority,
+        completion_date,
+        creation_date,
+        description: description.to_string(),
+    }))
+}
+
+/// Parse file of todo entries
+pub fn parse_file(input: &str) -> IResult<&str, Vec<TodoEntry>> {
+    let (leftover, x) = many0(terminated(
+        parse_entry,
+        line_ending
+    )).parse(input)?;
+
+    Ok((leftover, x))
 }
 
 #[cfg(test)]
@@ -125,17 +76,14 @@ mod tests {
 
     #[test]
     fn todo_entry_to_string() {
-        // TODO this is kinda useless atm
         let now = NaiveDate::from_ymd_opt(2026, 1, 1).unwrap();
-        let entry = TodoEntry::new(true, Some('Z'), Some(now), Some(now), "Some kind of +task @test location:italy".to_string());
-        // {
-        //     completed: true,
-        //     priority: Some('Z'),
-        //     completion_date: Some(now),
-        //     creation_date: Some(now),
-        //     raw_description: "Some kind of +task @test location:italy".to_string(),
-        //     ..Default::default()
-        // };
+        let entry = TodoEntry {
+            completed: true,
+            priority: Some('Z'),
+            completion_date: Some(now),
+            creation_date: Some(now),
+            description: "Some kind of +task @test location:italy".to_string()
+        };
 
         assert_eq!(Into::<String>::into(&entry), "x (Z) 2026-01-01 2026-01-01 Some kind of +task @test location:italy".to_string());
     }
@@ -147,10 +95,23 @@ mod tests {
 
         let (_, entry) = parse_entry("x (B) 2022-01-01 2021-01-01 Hello from +my dear @friends key:val\r\n").unwrap();
         let entry2 = TodoEntry {
-            raw_description: "smth".to_string(),
+            description: "smth".to_string(),
             ..Default::default()
         };
 
         assert_eq!(Into::<String>::into(entry), Into::<String>::into(entry2));
+    }
+
+    #[test]
+    fn todo_entry_parse_entry_file() {
+        let file = r#"x (B) Buy groceries +qol
+(C) Wash car +qol
+Do the +project_x with @alice
+
+        "#;
+
+        let (_, entries) = parse_file(file).unwrap();
+
+        // TODO
     }
 }
